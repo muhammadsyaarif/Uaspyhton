6f4973a22182f1b8de503e5b11ae3a3809299f92
uminurjanah09 <uminurjanah1209@gmail.com>
2023-11-10 15:04:50 +0700

    fasion

--- shopp.py
+++ shopp.py
@@ -1,7 +1,25 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import streamlit as st
+import numpy as np
+
+st.markdown(
+    """
+    <style>
+        body {
+            background-color: #FFEBCD;
+        }
+        .stApp {
+            max-width: 1200px;
+            margin: 0 auto;
+        }
+        .sidebar .sidebar-content {
+            background-color: #87CEEB;  /* Ganti dengan warna sidebar yang diinginkan */
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 # For example, add a title and some content
 st.title('Data Penjualan Pakaian Online Shop')
@@ -16,23 +34,23 @@ df3 = pd.read_csv('shopping_trends.csv')
 st.sidebar.title('Group Members')
 
 # List of group members
-group_members = ['Muhammad Syarif', 'Gentar Alam', 'Rizki Ikhza']
+group_members = ['Umi Nurjanah', 'Chaved', 'Nelson']
 
 # Create a dropdown to select a group member
 selected_member = st.sidebar.selectbox('Select Group Member', group_members)
 
 # Dictionary of group members' information
 members_info = {
-    'Muhammad Syarif': {
-        'Email': 'syrf253@gmail.com',
+    'Umi Nurjanah': {
+        'Email': 'uminurjanah@example.com',
         'Kelas': 'TI21A4',
     },
-    'Gentar Alam': {
-        'Email': 'gengengen.com',
+    'Chaved': {
+        'Email': 'chaved@example.com',
         'Kelas': 'TI21A4'
     },
-    'Rizki Ikhza': {
-        'Email': 'rizki190@gmail.com',
+    'Nelson': {
+        'Email': 'nelson@example.com',
         'Kelas': 'TI21A4'
     },
 }
@@ -63,7 +81,7 @@ for selected_file in selected_files:
 # Add a checkbox to control the display of the chart
 show_chart = st.sidebar.checkbox('Show Chart', value=True)
 
-# Create a pie chart to show the comparison of total purchases based on the checkbox value
+# Create a bar chart to show the comparison of total purchases based on the checkbox value
 if show_chart:
     total_purchase_file1 = df1['Purchase Amount (USD)'].sum()
     total_purchase_file2 = df2['Purchase Amount (USD)'].sum()
@@ -71,17 +89,19 @@ if show_chart:
 
     labels = ['File 1', 'File 2', 'File 3']
     sizes = [total_purchase_file1, total_purchase_file2, total_purchase_file3]
-    explode = (0.1, 0, 0)
 
+    # Plot the bar chart
     fig, ax = plt.subplots()
-    ax.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=140, explode=explode, colors=['#ff7f50', '#00ced1', '#98fb98'])
-    ax.axis('equal')
+    ax.bar(labels, sizes, color=['#ff7f50', '#00ced1', '#98fb98'])
+    ax.set_ylabel('Total Purchase Amount (USD)')
+    ax.set_xlabel('Files')
+    ax.set_title('Comparison of Total Purchases')
     st.sidebar.pyplot(fig)
 
 # Add a home page with an image and a description of online shop clothing sales data
 if not selected_files:
-    st.image('g1.jpg', use_column_width=True)
-    st.write('Data penjualan pakaian online shop. Pilih berkas data di sidebar untuk melihat informasi penjualan. Anda juga dapat menampilkan grafik perbandingan total pembelian.')
+    st.image('g3.jpg', use_column_width=True)
+    st.write('Data Penjualan fashion shop. Pilih barang fashion untuk melihat informasi penjualan. anda dapat memilih barang yang akan anda beli dan bisa melihat informasi apa saja penjualan yang ada disini.')
 
 # Run the Streamlit app
 if __name__ == '__main__':
